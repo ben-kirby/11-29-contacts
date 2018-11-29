bece25cd54792fc527f97be3e361ceffac9a7fe2
Ben Kirby and Robert Bruce <ben@>
2018-11-29 09:48:31 -0800

    Add basic contact methods and relavent tests

--- lib/Contact.rb
+++ lib/Contact.rb
@@ -1,5 +1,33 @@
 class Contact
+  attr_accessor :first_name, :last_name, :address, :id
+  @@contacts = []
 
+
+  def initialize(attributes)
+    @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @address = attributes.fetch(:address)
+    @id = @@contacts.length() +1
+  end
+
+  def save
+    @@contacts.push(self)
+  end
+
+  def self.all
+    @@contacts
+  end
+
+  def self.find(id)
+    list = all()
+    list.each do |i|
+      if i.id == id
+        return i
+      end
+    end
+  end
+
+  def self.clear
+    @@contacts = []
+  end
 end
-
-
--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -3,15 +3,37 @@ require ('pry')
 require ('Contact')
 
 
-describe('Contact') do 
-
-  it('tests a method for Contact') do
-
-    dummy = Contact.new()
-
-    expect(dummy.method()).to(eq(expected result))
-
+describe('Contact') do
+  before do
+    Contact.clear
   end
 
-end
+  describe('#save') do
+    it('tests a method for Contact') do
+      dummy = Contact.new({:first_name => "ben", :last_name=>"kirby", :address=> "portland"})
+      expect(dummy.save()).to(eq([dummy]))
+    end
+  end
 
+  describe('#all') do
+    it('reurns a list of all contacts') do
+      dummy = Contact.new({:first_name => "ben", :last_name=>"kirby", :address=> "portland"})
+      dummy.save()
+      dummy2 = Contact.new({:first_name => "rob", :last_name=>"bruce", :address=> "portland"})
+      dummy2.save()
+      expect(Contact.all()).to(eq([dummy, dummy2]))
+    end
+  end
+
+  describe('#find') do
+    it('reurns a list of all contacts') do
+      dummy = Contact.new({:first_name => "ben", :last_name=>"kirby", :address=> "portland"})
+      dummy.save()
+      dummy2 = Contact.new({:first_name => "rob", :last_name=>"bruce", :address=> "portland"})
+      dummy2.save()
+      expect(Contact.find(1)).to(eq(dummy))
+      expect(Contact.find(2)).to(eq(dummy2))
+
+    end
+  end
+end
